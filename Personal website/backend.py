acea5f3f028c514aaf48c054760f6e9ba8482da7
SushiMan <45275055+SushiMan-o3@users.noreply.github.com>
2021-01-05 12:32:37 -0500

    Update backend.py

--- Personal website/backend.py
+++ Personal website/backend.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html') 
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('about.html') 
 
 if __name__ == "__main__":
     app.run(debug = True)
